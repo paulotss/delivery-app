974c69c45860b4cdeba0eb0f774c186ef61aca5e
ThalesFerreira <thalesferreira190@gmail.com>
2022-11-29 17:41:00 -0300

    rota user testando

---
+++ back-end/src/controllers/user.controller.js
@@ -0,0 +1,47 @@
+class UserController {
+    constructor(service, req, res, next) {
+        this.req = req;
+        this.res = res;
+        this.next = next;
+        this.service = service;
+    };
+    async findAll() {
+        try {
+            const response = await this.service.findAll();
+            this.res.status(200).json(response)
+        } catch (error) {
+            this.next(error);
+        };
+    };
+
+    async create() {
+        try {
+            const response = await this.service.create(this.req.body);
+            this.res.status(201).json(response);
+        } catch (error) {
+            this.next(error);
+        };
+    };
+
+    async findById() {
+        try {
+            const { id } = this.req.params;
+            const response = await this.service.findById(id);
+            this.res.status(200).json(response);
+        } catch (error) {
+            this.next(error);
+        };
+    };
+
+    async deleteById(id) {
+        try {
+            const { id } = this.req.params;
+            const response = await this.service.deleteById(id);
+            this.res.status(204).json(response);
+        } catch (error) {
+            this.next(error);
+        };
+    };
+
+
+}
--- back-end/src/routes/user.js
+++ back-end/src/routes/user.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const userModel = require('../models/user');
+const UserService = require('../services/user.service');
+const UserController = require('../controllers/user.controller');
 
+const service = new UserService(userModel);
 const router = express.Router();
+
+routes.get('/', (req, res, next) => new UserController(service, req, res, next).findAll());
 
 module.exports = router;
