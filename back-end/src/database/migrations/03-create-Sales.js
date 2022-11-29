da7273e6194ef45e99a05616d7687e444415aa7d
ThalesFerreira <thalesferreira190@gmail.com>
2022-11-29 17:57:10 -0300

    vai dar conflito ctz

--- back-end/src/database/migrations/03-create-Sales.js
+++ back-end/src/database/migrations/03-create-Sales.js
@@ -54,7 +54,7 @@ module.exports = {
     });
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface, _Sequelize) => {
     await queryInterface.dropTable("products");
   },
 };
