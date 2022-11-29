b1e5bfc15ce8d55f0794442141b10fee47768af8
Lucas <lucaslopes@gea.inatel.br>
2022-11-29 18:24:01 -0300

    v1 migrations

--- back-end/src/routes/user.js
+++ back-end/src/routes/user.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const userModel = require('../models/user');
+const {User} = require('../database/models');
+
+console.log(User);
 const UserService = require('../services/user.service');
 const UserController = require('../controllers/user.controller');
 
-const service = new UserService(userModel);
+
+
+const service = new UserService(User);
 const router = express.Router();
 
-routes.get('/', (req, res, next) => new UserController(service, req, res, next).findAll());
+router.get('/', (req, res, next) => new UserController(service, req, res, next).findAll());
 
 module.exports = router;
